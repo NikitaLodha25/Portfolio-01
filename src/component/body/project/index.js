d73c332a390697079069758971cd5b115ebac95b
System GitHub <nikitalodha25@gmail.com>
2022-01-11 16:36:01 +0530

    second commit

--- src/component/body/project/index.js
+++ src/component/body/project/index.js
@@ -11,8 +11,8 @@ function Project() {
             title:"Random Project 1: Flipkart clone",
             about:"This a random project which I have been experimenting to get better grip on frontend.I just tried to create a Flipkart clone home page. And I am also working on this page currently, to give it a better look and add more options.",
             tags:["HTML","CSS","Js"],
-            demo:"https://www.example.com",
-            github:'https://www.github.com',
+            demo:"https://nikitalodha25.github.io/Flipkart-clone/",
+            github:'https://nikitalodha25.github.io/Flipkart-clone/',
             image:img1,
         },
         {
@@ -20,8 +20,8 @@ function Project() {
             title:"Random Project 2: Spotify clone",
             about:"This a random project which I have been experimenting to get better grip on frontend.This a clone of Spotify which contains my favourite songs, I have added 10 songs here which can be played in a loop.",
             tags:["HTML","CSS","Js"],
-            demo:"https://www.example.com",
-            github:'https://www.github.com',
+            demo:" https://nikitalodha25.github.io/Spotify-clone/",
+            github:' https://nikitalodha25.github.io/Spotify-clone/',
             image:img2,
         },
     ];
